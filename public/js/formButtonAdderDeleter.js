5378cbc2bda5367ecd651c559a515e52c72ae22c
David Terwilliger <terwilld@gmail.com>
2024-05-24 13:55:33 -0400

    added event listeners for form, delete and add rows

--- public/js/formButtonAdderDeleter.js
+++ public/js/formButtonAdderDeleter.js
@@ -1,29 +1,75 @@
 window.addEventListener("load", function () {
     document.getElementById("rowAdder").onclick = function () {
         /*do something*/
-        console.log("derp")
+        console.log("clicking the add row button")
 
 
+        let topDiv = document.createElement("div")
+        topDiv.classList = "col-md-12 d-flex flex-row"
+        let inputGroup = document.createElement("div")
+        inputGroup.classList = "input-group mb-3"
 
-        newRowAdd = '<div id="row"> <div class="input-group m-3"></div>'
+        let itemNameInput = document.createElement("input")
+        itemNameInput.type = "text"
+        itemNameInput.classList = "form-control"
+        itemNameInput.placeholder = "Item Name"
+        itemNameInput.setAttribute('aria-label', 'Username')
+        itemNameInput.require = true
+        itemNameInput.setAttribute('name', 'customer[itemName]')
+
+        let itemValueInput = document.createElement("input")
+        itemValueInput.type = "text"
+        itemValueInput.classList = "form-control"
+        itemValueInput.placeholder = "Item Name"
+        itemValueInput.setAttribute('aria-label', 'Item Value')
+        itemValueInput.require = true
+        itemValueInput.setAttribute('name', 'customer[itemValue]')
+
+        let btn = document.createElement("button")
+        btn.classList = "btn btn-danger btn-lg deleteRow"
+
+        let trashcan = document.createElement("i")
+        trashcan.classList = "bi bi-trash3"
+        btn.appendChild(trashcan)
+        btn.addEventListener('click', event => {
+            event.preventDefault()
+            console.log(event.target.getAttribute("data-el"));
+            console.log('derp')
+            event.currentTarget.parentNode.parentNode.remove();
+        })
+
+        inputGroup.appendChild(itemNameInput)
+        inputGroup.appendChild(itemValueInput)
+        inputGroup.appendChild(btn)
+        topDiv.appendChild(inputGroup)
 
 
-
-        newRowAdd =
-            '<div id="row"> <div class="input-group m-3">' +
-            '<div class="input-group-prepend">' +
-            '<button class="btn btn-danger" id="DeleteRow" type="button">' +
-            '<i class="bi bi-trash"></i> Delete</button> </div>' +
-            '<input type="text" class="form-control m-input"> </div> </div>';
-
-
-
-
-
-
-
-
-
+        this.parentNode.insertAdjacentElement("beforebegin", topDiv)
 
     };
+
+
 });
+
+
+const buttons = document.querySelectorAll('.deleteRow');
+
+buttons.forEach(el => el.addEventListener('click', event => {
+    event.preventDefault()
+    console.log(event.target.getAttribute("data-el"));
+    console.log('derp')
+    event.currentTarget.parentNode.parentNode.remove();
+
+}));
+
+
+
+var myremoveFunction = function (event) {
+    event.preventDefault()
+    console.log(event.target.getAttribute("data-el"));
+    console.log('derp')
+    event.currentTarget.parentNode.remove();
+}
+
+
+
